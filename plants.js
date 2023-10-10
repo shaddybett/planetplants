f515500f7c79a1ee8a807cc73778d984f3fcd169
Shaddy <shadybett540@gmail.com>
2023-10-10 23:29:13 +0300

    about to finish

--- plants.js
+++ plants.js
@@ -1,1 +1,12 @@
-console.log('Hello, world!');
+let page = 1;
+
+let paginationEl = document.getElementById('pagination');
+
+let paginationBtns = paginationEl.getElementsByTagName('button');
+
+paginationBtns[0].addEventListener('click',goBack );
+
+function goBack (){
+    console.log('previous');
+}
+
